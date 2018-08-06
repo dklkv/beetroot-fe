a1f4dc8eced986b460d17d7aef2b4701acf9afc0
Dmitriy <dima@bytesoft.io>
2018-08-06 11:35:14 +0300

    Foliac - contact form validation

--- foliac/js/script.js
+++ foliac/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    //Smooth scrolling
     var $anchors = $('a[href^="#"]').not('[href="#"]');
 
     $anchors.click(function (e) {
@@ -12,12 +13,14 @@ $(document).ready(function () {
         }, 500);
     });
 
+    //Slick-slider
     $('#slider').slick({
         autoplay: true,
         arrows: false,
         mobileFirst: true
     });
 
+    //Isotope
     var $isotopeGrid = $('.portfolio-images');
 
     $isotopeGrid.isotope({
@@ -32,4 +35,56 @@ $(document).ready(function () {
             filter: filterValue
         });
     });
+
+    //Validation
+    var form = document.forms.contact;
+
+    $(form).submit(function (e) {
+        $('.error').empty();
+
+        var inputs = form.elements;
+
+        for(var i = 0; i < inputs.length; i++) {
+            var error = '';
+
+            if (inputs[i].type === 'text' || inputs[i].tagName === 'TEXTAREA') {
+                error = validateText(inputs[i]);
+            } else if (inputs[i].type === 'email') {
+                error = validateEmail(inputs[i]);
+            }
+
+            if (error) {
+                $(inputs[i]).closest('.input-and-error-wrapper').find('.error').text(error);
+            }
+        }
+
+        if ($('.error:parent').length > 0) {
+            e.preventDefault();
+        }
+    });
+
+    function validateText(input) {
+        var min = input.dataset.min,
+            max = input.dataset.max,
+            value =input.value.trim();
+
+        if (value.length < min) {
+            return 'Your input should be not less than ' + min + ' symbols.';
+        } else if (value.length > max) {
+            return 'Your input should be not longer than ' + max + ' symbols.';
+        } else {
+            return '';
+        }
+    }
+
+    function validateEmail(input) {
+        var emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
+            value = input.value.trim();
+
+        if (emailRegexp.test(value)) {
+            return '';
+        } else {
+            return 'This is not valid email.';
+        }
+    }
 });
